5ca1b84a5ba9dff7b23b02ce17810ddc17db92e9
Miriam <m.corchero.blazquez@accenture.com>
2023-01-18 11:52:15 +0100

    modificacion ejercicio DatosArray con bucles

--- Definiciones/src/es/rf/curso/base/DatosArray.java
+++ Definiciones/src/es/rf/curso/base/DatosArray.java
@@ -6,14 +6,25 @@ public class DatosArray {
 		int[] diaMes = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 		int[][] dimensiones = new int[5][7];
 		int valor = 1;
-		
+		/*
 		dimensiones[0][0] = 1;
 		dimensiones[0][1] = 2;
 		dimensiones[0][2] = 3;
 		dimensiones[0][3] = 4;
 		//...
 		dimensiones[1][0] = 8;
-		//...
+		//...*/
+		
+		for(int i = 0; i<dimensiones.length; i++) {
+			for(int j = 0; j<dimensiones[i].length; j++) {
+				if(valor <= 31) {
+					dimensiones[i][j] = valor;
+					valor++;
+				}
+				System.out.print("|\t" + dimensiones[i][j] + "\t|");
+			}
+			System.out.println("");
+		}
 
 	}
 
