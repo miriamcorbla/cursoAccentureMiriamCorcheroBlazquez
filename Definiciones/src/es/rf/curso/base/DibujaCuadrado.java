5759969a071f8a10a973ac0b2e5d3dff45ff0030
Miriam <m.corchero.blazquez@accenture.com>
2023-01-19 11:55:28 +0100

    Finalizada tanda de ejercicios de Bucles y Saltos

---
+++ Definiciones/src/es/rf/curso/base/DibujaCuadrado.java
@@ -0,0 +1,14 @@
+package es.rf.curso.base;
+
+public class DibujaCuadrado {
+
+	public static void main(String[] args) {
+		int tamLado = 8; //tamaño del lado
+		for(int i = 0; i<=tamLado; i++) {
+			for(int j = 0; j<tamLado; j++) {
+				System.out.print("* "); //OJO antes puse println y esto salta de linea, para que no ocurra: print
+			}
+			System.out.println(" ");
+		}
+	}
+}
