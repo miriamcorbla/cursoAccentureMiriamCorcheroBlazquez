d40f4aba2efbc64be0819edf399e7f6e0140beb8
Miriam <m.corchero.blazquez@accenture.com>
2023-01-20 22:43:31 +0100

    Añadiendo módulo de los ejercicios del tema de Excepciones

---
+++ Semana2/src/excepciones/SegundoEjercicio.java
@@ -0,0 +1,23 @@
+package excepciones;
+
+/**
+ * Clase que define las variables a y b y calcula el cociente de b/a.
+ * Controla las excepciones pasándolo al módulo
+ * llamador y siempre muestra por pantalla un mensaje
+ * al finalizar el programa
+ * @author m.corchero.blazquez
+ * @version 1.0
+ */
+public class SegundoEjercicio {
+	public static void main(String[] args) throws Exception{
+		int a = 0;
+		int b = 300;
+		int result;
+		try {
+			result = b / a;
+			System.out.println(result);
+		}finally{
+			System.out.println("Programa Finalizado");
+		}
+	}
+}
