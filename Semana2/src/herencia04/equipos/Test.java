851e63440d42a8d9e341b93ed02e25f0c5190f03
Miriam <m.corchero.blazquez@accenture.com>
2023-01-23 11:29:45 +0100

    Añadiendo corrección en la clase Test

--- Semana2/src/herencia04/equipos/Test.java
+++ Semana2/src/herencia04/equipos/Test.java
@@ -11,24 +11,24 @@ public class Test {
 
 	public static void main(String[] args) {
 		ArrayList<Profesion> integrantes = new ArrayList<>();
-		Futbolista integrante1 = new Futbolista(1, "Lucia", "Gomez", 20, 123, "delantero");
-		integrantes.add(integrante1);
-		Entrenador integrante2 = new Entrenador(2, "Carla", "Gonzalez", 34, 0231);
-		integrantes.add(integrante2);
-		Masajista integrante3 = new Masajista(3, "Lucas", "Rodriguez", 40, "Grado en Fisioterapia", 18);
-		integrantes.add(integrante3);
+		integrantes.add( new Futbolista(1, "Lucia", "Gomez", 20, 123, "delantero"));
+		integrantes.add(new Entrenador(2, "Carla", "Gonzalez", 34, 0231));
+		integrantes.add(new Masajista(3, "Lucas", "Rodriguez", 40, "Grado en Fisioterapia", 18));
 		
 		for(Profesion p : integrantes) {
 			p.concentrarse();
 			p.viajar();
+			if(p instanceof Futbolista) {
+				((Futbolista) p).jugarPartido();
+			}
+			if(p instanceof Entrenador) {
+				((Entrenador)p).dirigirEntrenamiento();
+				((Entrenador)p).dirigirPartido();
+			}
+			if(p instanceof Masajista) {
+				((Masajista) p).darMasaje();
+			}
 		}
-		
-		integrante1.jugarPartido();
-		integrante1.entrenar();
-		integrante2.dirigirPartido();
-		integrante2.dirigirEntrenamiento();
-		integrante3.darMasaje();
-		
 	}
 
 }
