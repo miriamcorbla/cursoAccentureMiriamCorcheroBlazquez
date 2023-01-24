8c6da7e184bc676f07fb17bcce9300afe3b145c8
Miriam <m.corchero.blazquez@accenture.com>
2023-01-24 11:43:34 +0100

    Nuevo proy. Copia y modificando ficheros de Ej. Cuentas de Excepciones

---
+++ Banco/src/banco/clases/Movimiento.java
@@ -0,0 +1,59 @@
+package banco.clases;
+
+import java.time.LocalDate;
+
+/**
+ * POJO Movimiento
+ * @author m.corchero.blazquez
+ *
+ */
+public class Movimiento {
+	
+	private String mConcepto;
+	private LocalDate mFecha;
+	private double mImporte;
+	
+	/**
+	 * Método observador que devuelve el concepto
+	 * @return mConcepto
+	 */
+	public String getmConcepto() {
+		return mConcepto;
+	}
+	/**
+	 * Método modificador para el atributo mConcepto
+	 * @param mConcepto
+	 */
+	public void setmConcepto(String mConcepto) {
+		this.mConcepto = mConcepto;
+	}
+	/**
+	 * Método observador para el atributo fecha
+	 * @return mFecha
+	 */
+	public LocalDate getmFecha() {
+		return mFecha;
+	}
+	/**
+	 * Método modificador para el atributo fecha
+	 * @param mFecha
+	 */
+	public void setmFecha(LocalDate mFecha) {
+		this.mFecha = mFecha;
+	}
+	/**
+	 * Método observador para el atributo importe
+	 * @return
+	 */
+	public double getmImporte() {
+		return mImporte;
+	}
+	/**
+	 * Método modificador del atributo importe
+	 * @param mImporte
+	 */
+	public void setmImporte(double mImporte) {
+		this.mImporte = mImporte;
+	}
+
+}
